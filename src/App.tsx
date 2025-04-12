cf53322da19505a6cfd9df485ad5ed67c0e458db
Narotam Das <narotamdas98@gmail.com>
2025-04-12 21:26:42 +0500

    import fixed of speed inshgts

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Contact from "@components/Contact";
 import Footer from "@components/Footer";
 
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
